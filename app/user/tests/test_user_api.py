3d32fec883eefe9a4d29f64ab6a9733150eea192
Pattanao <pat.sutanc@gmail.com>
2025-05-14 22:23:07 +0700

    feat(user): implement API for user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -54,7 +54,7 @@ class PublicUserAPITests(TestCase):
         """Test an error is returned if password less than 5 chars."""
         payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
+            'password': 'pw',
             'name': 'Test name'
         }
         res = self.client.post(CREATE_USER_URL, payload)
