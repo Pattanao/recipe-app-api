28d6137672bc3644a981fd4637c9d022cb87b2b4
Pattanao <pat.sutanc@gmail.com>
2025-05-27 21:45:56 +0700

    feat: implement view for delete ingredient api

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -92,5 +92,5 @@ class PrivateIngredientsAPITests(TestCase):
         res = self.client.delete(url)
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-        ingredients = Ingredient.objects.all(user=self.user)
+        ingredients = Ingredient.objects.filter(user=self.user)
         self.assertFalse(ingredients.exists())
